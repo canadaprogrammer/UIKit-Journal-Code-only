8fdac8e8d6be6e72237e2e68355b877414f54a7c
Jin Park <jinpark1504@gmail.com>
2024-05-13 17:42:15 +0900

    Add StackView to New Entry (finish)

--- JRN-CodeOnly/AddJournalViewController.swift
+++ JRN-CodeOnly/AddJournalViewController.swift
@@ -40,7 +40,24 @@ class AddJournalViewController: UIViewController {
         
         return stackView
     }()
-                                        
+    private lazy var titleTextField: UITextField = {
+        let textField = UITextField()
+        textField.placeholder = "Journal Title"
+        return textField
+    }()
+
+    private lazy var bodyTextView: UITextView = {
+        let textView = UITextView()
+        textView.text = "Journal Boddy"
+        return textView
+    }()
+
+    private lazy var imageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.image = UIImage(systemName: "face.smiling")
+        return imageView
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -53,12 +70,20 @@ class AddJournalViewController: UIViewController {
         
         mainContainer.addArrangedSubview(ratingView)
         mainContainer.addArrangedSubview(toggleView)
+        mainContainer.addArrangedSubview(titleTextField)
+        mainContainer.addArrangedSubview(bodyTextView)
+        mainContainer.addArrangedSubview(imageView)
+        
         view.addSubview(mainContainer)
         
         let safeArea = view.safeAreaLayoutGuide
         
         mainContainer.translatesAutoresizingMaskIntoConstraints = false
         ratingView.translatesAutoresizingMaskIntoConstraints = false
+        toggleView.translatesAutoresizingMaskIntoConstraints = false
+        titleTextField.translatesAutoresizingMaskIntoConstraints = false
+        bodyTextView.translatesAutoresizingMaskIntoConstraints = false
+        imageView.translatesAutoresizingMaskIntoConstraints = false
 
         
         NSLayoutConstraint.activate([
@@ -67,7 +92,17 @@ class AddJournalViewController: UIViewController {
             mainContainer.trailingAnchor.constraint(equalTo: safeArea.trailingAnchor, constant: -20),
             
             ratingView.widthAnchor.constraint(equalToConstant: 252),
-            ratingView.heightAnchor.constraint(equalToConstant: 44)
+            ratingView.heightAnchor.constraint(equalToConstant: 44),
+            
+            titleTextField.leadingAnchor.constraint(equalTo: mainContainer.leadingAnchor, constant: 8),
+            titleTextField.trailingAnchor.constraint(equalTo: mainContainer.trailingAnchor, constant: -8),
+            
+            bodyTextView.leadingAnchor.constraint(equalTo: mainContainer.leadingAnchor, constant: 8),
+            bodyTextView.trailingAnchor.constraint(equalTo: mainContainer.trailingAnchor, constant: -8),
+            bodyTextView.heightAnchor.constraint(equalToConstant: 128),
+            
+            imageView.widthAnchor.constraint(equalToConstant: 200),
+            imageView.heightAnchor.constraint(equalToConstant: 200)
         ])
     }
     
