6aeb89ce603fc681ba6ad07400bf1182f7cc679c
Jin Park <jinpark1504@gmail.com>
2024-05-13 11:03:33 +0900

    Create AddJournalViewController and add a navigation BarButtonItem

---
+++ JRN-CodeOnly/AddJournalViewController.swift
@@ -0,0 +1,31 @@
+//
+//  AddJournalViewController.swift
+//  JRN-CodeOnly
+//
+//  Created by Jungjin Park on 2024-05-13.
+//
+
+import UIKit
+
+class AddJournalViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        navigationItem.title = "New Entry"
+        view.backgroundColor = .white
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
