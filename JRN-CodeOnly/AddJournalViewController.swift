183b609e0b4c9813fbde0425ec8c5c76337aa6c1
Jin Park <jinpark1504@gmail.com>
2024-05-17 10:33:12 +0900

    add current location to Journal Entry

--- JRN-CodeOnly/AddJournalViewController.swift
+++ JRN-CodeOnly/AddJournalViewController.swift
@@ -6,13 +6,19 @@
 //
 
 import UIKit
+import CoreLocation
 
 protocol AddJournalControllerDelegate: NSObject {
     func saveJournalEntry(_ journalEntry: JournalEntry)
 }
 
-class AddJournalViewController: UIViewController {
+class AddJournalViewController: UIViewController, CLLocationManagerDelegate {
     weak var delegate: AddJournalControllerDelegate?
+    
+    final let LABEL_VIEW_TAG = 1001
+    
+    let locationManager = CLLocationManager()
+    var currentLocation: CLLocation?
     
     private lazy var mainContainer: UIStackView = {
         let stackView = UIStackView()
@@ -38,8 +44,14 @@ class AddJournalViewController: UIViewController {
         stackView.spacing = 8
         
         let switchComponent = UISwitch()
+        // set switch action
+        switchComponent.isOn = false
+        switchComponent.addTarget(self, action: #selector(valueChanged(sender:)), for: .valueChanged)
+        
         let labelComponent = UILabel()
-        labelComponent.text = "Switch Label"
+        labelComponent.text = "Get Location"
+        // set tag to change the label
+        labelComponent.tag = LABEL_VIEW_TAG
         stackView.addArrangedSubview(switchComponent)
         stackView.addArrangedSubview(labelComponent)
         
@@ -109,12 +121,20 @@ class AddJournalViewController: UIViewController {
             imageView.widthAnchor.constraint(equalToConstant: 200),
             imageView.heightAnchor.constraint(equalToConstant: 200)
         ])
+        
+        locationManager.delegate = self
+        locationManager.requestAlwaysAuthorization()
     }
     
     @objc func save() {
         guard let title = titleTextField.text, !title.isEmpty,
               let body = bodyTextView.text, !body.isEmpty else { return }
-        let journalEntry = JournalEntry(rating: 3, title: title, body: body, photo: UIImage(systemName: "face.smiling"))!
+        // add current location to Journal Entry
+        let lat = currentLocation?.coordinate.latitude
+        let long = currentLocation?.coordinate.longitude
+        
+        let journalEntry = JournalEntry(rating: 3, title: title, body: body, photo: UIImage(systemName: "face.smiling"), latitude: lat, longitude: long)!
+        
         delegate?.saveJournalEntry(journalEntry)
         dismiss(animated: true)
     }
@@ -123,14 +143,32 @@ class AddJournalViewController: UIViewController {
         dismiss(animated: true)
     }
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @objc func valueChanged(sender: UISwitch) {
+        if sender.isOn {
+            if let label = toggleView.viewWithTag(LABEL_VIEW_TAG) as? UILabel {
+                label.text = "Getting Location..."
+            }
+            locationManager.requestLocation()
+        } else {
+            currentLocation = nil
+            if let label = toggleView.viewWithTag(LABEL_VIEW_TAG) as? UILabel {
+                label.text = "Get Location"
+            }
+        }
     }
-    */
-
+    
+    // MARK: - CLLocationManagerDelegate
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        if let myCurrentLocation = locations.first {
+            currentLocation = myCurrentLocation
+            if let label = toggleView.viewWithTag(LABEL_VIEW_TAG) as? UILabel {
+                label.text = "Done"
+            }
+            // TODO: updateButtonState
+        }
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: any Error) {
+        print("Failed to find user's location: \(error.localizedDescription)")
+    }
 }
